c6a7ece386934e258b8e2c62d114b307a8904320
Hika Hibariya <hibariya@gmail.com>
2015-10-11 09:30:59 +0900

    Care external link

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+var completeLinkTarget = function() {
+  var links = document.getElementsByTagName('a');
+
+  for (i = 0; i < links.length; i++) {
+    var link = links[i];
+
+    if (!link.getAttribute('href').match('^https?:') || link.getAttribute('target')) continue;
+
+    link.setAttribute('target', '_blank');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', completeLinkTarget, false);
